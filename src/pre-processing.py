fc566ac9c608a7ccaed675e3513d21788cdc6379
aromatic-toast <iamthelm@gmail.com>
2020-11-11 12:28:54 -0800

    added the train set to be cleaned

--- src/pre-processing.py
+++ src/pre-processing.py
@@ -18,13 +18,26 @@ valid = pd.read_csv("data/Valid.csv")
 test = pd.read_csv("data/Test.csv")
 y_test = test.label
 
-#########------------VALIDATION SET------------##########33
+#########------------TRAIN SET------------##########
+# pre-process the train text
+print("Removing html on train...")
+train_no_html = imdb.remove_html(train.text)
+
+# clean the train set
+print("cleaning train...")
+train_clean = imdb.clean_text(train_no_html)
+
+# create clean train df
+train_clean_dict = {'text':train_clean, 'label': train.label}
+train_clean_df = pd.DataFrame(train_clean_dict)
+
+#########------------VALIDATION SET------------##########
 # pre-process the validation text
-print("Removing html...")
+print("Removing html on validation...")
 valid_no_html = imdb.remove_html(valid.text)
 
 # clean the validation set
-print("cleaning corpus...")
+print("cleaning validation...")
 valid_clean = imdb.clean_text(valid_no_html)
 
 # create clean validation df
@@ -34,11 +47,11 @@ valid_clean_df = pd.DataFrame(valid_clean_dict)
 
 #########------------TEST SET------------##########33
 # pre-process the test text
-print("Removing html...")
+print("Removing html on test...")
 test_no_html = imdb.remove_html(test.text)
 
 # clean the test set
-print("cleaning corpus...")
+print("cleaning test...")
 test_clean = imdb.clean_text(valid_no_html)
 
 # create clean test df
@@ -46,7 +59,8 @@ test_clean_dict = {'text': test_clean, 'label': test.label}
 test_clean_df = pd.DataFrame(test_clean_dict)
 
 
-print("writing out cleaned validation & test sets to csv")
+print("writing out cleaned train, validation & test sets to csv")
+train_clean_df.to_csv('data/train_clean.csv')
 valid_clean_df.to_csv('data/valid_clean.csv')
 test_clean_df.to_csv('data/test_clean.csv')
 
