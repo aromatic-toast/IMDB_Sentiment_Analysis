4ed4cf44b16a63929bfbf93892e1c4dbf02a0993
aromatic-toast <iamthelm@gmail.com>
2020-11-11 10:39:21 -0800

    changed the d2v model to be the model that was built using cleaned train text and added a line to extract the train set vectors and write them out

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -12,22 +12,32 @@ from gensim.models.doc2vec import Doc2Vec
 
 
 # load d2v model
-model = Doc2Vec.load("results/d2v.model")
+model = Doc2Vec.load("results/d2v_X_train_clean.model")
+
+# extract the train set vectors
+print("extracting train set vectors...")
+train_df = imdb.extract_doc_vecs(model=model, vec_size=100, infer=False)
 
 # load cleaned validation set
+print("loading and tokenizing validation set...")
 valid = pd.read_csv("data/valid_clean.csv")
 valid = imdb.tokenize(valid.text)
 
 # load clean test set
+print("loading and tokenizing test set...")
 test = pd.read_csv("data/test_clean.csv")
 test = imdb.tokenize(test.text)
 
 # infer doc vectors for validation set
+print("inferring validation vectors...")
 valid_df = imdb.extract_doc_vecs(model=model, vec_size=100, infer=True, docs=valid)
 
 # infer doc vectors for test set
+print("inferring test vectors")
 test_df = imdb.extract_doc_vecs(model=model, vec_size=100, infer=True, docs=test)
 
 # write out the extracted vectors as csv
+print('writing out vectorized datasets...')
+train_df.to_csv("data/train_d2v.csv")
 valid_df.to_csv("data/valid_d2v.csv")
 test_df.to_csv("data/test_d2v.csv")
